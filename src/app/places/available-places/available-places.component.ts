de2adde68ad690c2fd7bd68470d4d6ae1a8d3020
raseward14 <raseward14@gmail.com>
2026-03-23 18:04:04 -0600

    added type definition for the get request with the place model

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -20,9 +20,9 @@ export class AvailablePlacesComponent implements OnInit {
   // constructor(private httpClient: HttpClient) {}
 
   ngOnInit() {
-    const subscription = this.httpClient.get('http://localhost:3000/places').subscribe({
+    const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places').subscribe({
       next: (resData) => {
-        console.log(resData)
+        console.log(resData.places)
       }
     });
 
