9668ffac5f051ef0a3c52b54d32a87d9217b929a
raseward14 <raseward14@gmail.com>
2026-04-06 13:12:07 -0600

    moved user places to a service signal with the pipe and tap operator

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable, signal } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { map, catchError, throwError } from "rxjs";
+import { map, catchError, throwError, tap } from "rxjs";
 
 import { Place } from "./place.model";
 
@@ -24,7 +24,11 @@ export class PlacesService {
     return this.fetchPlaces(
       "http://localhost:3000/user-places",
       "Something went wrong fetching your favorite places. Please try again later.",
-    );
+    ).pipe(
+      tap({
+        next: (userPlaces) => this.userPlaces.set(userPlaces)
+      })
+    );  
   }
 
   addPlaceToUserPlaces(placeId: string) {
