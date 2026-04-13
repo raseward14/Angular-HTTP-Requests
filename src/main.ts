3728247bfe962543fccba638d2a10b161b59b16f
raseward14 <raseward14@gmail.com>
2026-04-13 16:40:38 -0600

    added type annotations for interceptor arguments

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { AppComponent } from './app/app.component';
 
-function loggingInterceptor() {
+function loggingInterceptor(request: HttpRequest, next: HttpHandlerFn) {
 
 }
 
