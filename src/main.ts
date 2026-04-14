f3b5cab9b82f6c0dfdd1ce78721604ccfd1b6bb7
raseward14 <raseward14@gmail.com>
2026-04-14 14:37:42 -0600

    added logging to loggingInterceptor and returned next with the request object - also tested cloning a request and setting an additional x-debug header

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,22 @@ import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from 
 
 import { AppComponent } from './app/app.component';
 
-function loggingInterceptor(request: HttpRequest, next: HttpHandlerFn) {
-
+function loggingInterceptor(
+    request: HttpRequest<unknown>,
+    next: HttpHandlerFn
+) {
+    // const req = request.clone({
+    //     headers: request.headers.set('X-DEBUG', 'TESTING')
+    // });
+    console.log('[Outgoing Request]');
+    console.log(request);
+    return next(request);
 }
 
 bootstrapApplication(AppComponent, {
     providers: [provideHttpClient(
-        withInterceptors([])
+        withInterceptors([
+            loggingInterceptor
+        ])
     )]
 }).catch((err) => console.error(err));
