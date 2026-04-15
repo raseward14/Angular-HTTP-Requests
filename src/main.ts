a9c4c9a4e4e3cced5f233d7e194ea0972600421c
raseward14 <raseward14@gmail.com>
2026-04-15 15:59:24 -0600

    logged response data using response interceptors via pipe tap and next to check if the event.type is HttpEventType.Response

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,50 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import {
     HTTP_INTERCEPTORS,
+    HttpEventType,
     HttpHandlerFn,
     HttpRequest,
     provideHttpClient,
     withInterceptors,
     withInterceptorsFromDi
 } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 import { AppComponent } from './app/app.component';
 
-import { LoggingInterceptor } from './loggingInterceptor';
+// import { LoggingInterceptor } from './loggingInterceptor';
 
-// function loggingInterceptor(
-//     request: HttpRequest<unknown>,
-//     next: HttpHandlerFn
-// ) {
-//     // const req = request.clone({
-//     //     headers: request.headers.set('X-DEBUG', 'TESTING')
-//     // });
-//     console.log('[Outgoing Request]');
-//     console.log(request);
-//     return next(request);
-// }
+function loggingInterceptor(
+    request: HttpRequest<unknown>,
+    next: HttpHandlerFn
+) {
+    // const req = request.clone({
+    //     headers: request.headers.set('X-DEBUG', 'TESTING')
+    // });
+    console.log('[Outgoing Request]');
+    console.log(request);
+    return next(request).pipe(
+        tap({
+            next: event => {
+                if (event.type === HttpEventType.Response) {
+                    console.log('[Incoming Response]');
+                    console.log(event.status);
+                    console.log(event.body);
+                }
+            }
+        })
+    );
+}
 
 bootstrapApplication(AppComponent, {
-    // providers: [provideHttpClient(
-    //     withInterceptors([
-    //         loggingInterceptor
-    //     ])
-    // )]
     providers: [provideHttpClient(
-        withInterceptorsFromDi()
-    ),
-    { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true }
-    ]
+        withInterceptors([
+            loggingInterceptor
+        ])
+    )]
+    // providers: [provideHttpClient(
+    //     withInterceptorsFromDi()
+    // ),
+    // { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true }
+    // ]
 }).catch((err) => console.error(err));
